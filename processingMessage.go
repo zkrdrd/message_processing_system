dea43eda58437b18cdae0fa731489a433e77bdf2
dev_delphin <eduard.zakirov.96@mail.ru>
2024-05-08 20:33:45 +0500

    struct add

--- processingMessage.go
+++ processingMessage.go
@@ -1,8 +1,13 @@
 package messageProcessingSystem
 
 type Message struct {
+	TypeMessage string `json:"Type"`
+	UidMessage  string `json:"Uid"`
+	AddressFrom string `json:"AddressFrom,omitempty"`
+	AddressTo   string `json:"AddressTo,omitempty"`
+	Payment     int    `json:"Payment,omitempty"`
 }
 
-func (mes *Message) Processing() {
+func (mes *Message) Processing(FileName string) {
 
 }
