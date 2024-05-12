7655ec35be98b46c5b988fd098b358935adc75c4
dev_delphin <eduard.zakirov.96@mail.ru>
2024-05-12 19:29:05 +0500

    create map

--- cmd/main.go
+++ cmd/main.go
@@ -5,17 +5,18 @@ import (
 	"messageProcessingSystem"
 )
 
-type Message struct {
+/*type Message struct {
 	TypeMessage string `json:"TypeMesage"`
 	UidMessage  string `json:"UidMessage"`
 	AddressFrom string `json:"AddressFrom,omitempty"`
 	AddressTo   string `json:"AddressTo,omitempty"`
 	Payment     int    `json:"Payment,omitempty"`
-}
+}*/
 
 func main() {
-	var msg = &Message{}
-	if err := messageProcessingSystem.Processing("messages/path1.json", msg); err != nil {
+	//var msg = &Message{}
+	var msg map[string]interface{}
+	if err := messageProcessingSystem.Processing("messages/path1.json", &msg); err != nil {
 		fmt.Println(err)
 	} else {
 		fmt.Println(msg)
--- processingMessage.go
+++ processingMessage.go
@@ -5,15 +5,20 @@ import (
 	"os"
 )
 
+// обработка json файлов
 func Processing(FileName string, config any) error {
+
 	readData, err := os.ReadFile(FileName)
 	if err != nil {
 		return err
 	}
 
+	/*if err := json.Unmarshal(readData, config); err != nil {
+		return err
+	}*/
+
 	if err := json.Unmarshal(readData, config); err != nil {
 		return err
 	}
-
 	return nil
 }
