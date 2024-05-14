4d16bbc1e1db253a77e202a28e3329b1fe63214d
dev_delphin <eduard.zakirov.96@mail.ru>
2024-05-14 22:03:00 +0500

    db add

--- internal/process/processingMessage.go
+++ internal/process/processingMessage.go
@@ -1,8 +1,22 @@
 package messageProcessingSystem
 
 import (
+	"log"
+	"messageProcessingSystem/internal/database"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/zkrdrd/ConfigParser"
 )
+
+func init() {
+	f, err := os.Create("storage/lite/message.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	database.DBcreate()
+}
 
 // обработка json файлов
 func Processing(FileName string, config any) error {
@@ -10,5 +24,7 @@ func Processing(FileName string, config any) error {
 	if err := ConfigParser.Read(FileName, config); err != nil {
 		return err
 	}
+	database.DBinsert()
+
 	return nil
 }
