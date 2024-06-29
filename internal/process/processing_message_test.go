a61106875f5d7f0e8d7560d27284f67a132f6bab
dev_delphin <eduard.zakirov.96@mail.ru>
2024-06-29 22:09:25 +0500

    add tests

--- internal/process/processing_message_test.go
+++ internal/process/processing_message_test.go
@@ -1,6 +1,7 @@
 package process_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"messageProcessingSystem/internal/process"
 	"messageProcessingSystem/model"
@@ -8,18 +9,29 @@ import (
 	"testing"
 )
 
+var storage = memory.NewDatabase()
+
 func TestPaymentProcessor(t *testing.T) {
 	t.Run(`test find min number`, func(t *testing.T) {
 		testTable := TestParam
 
-		pr := process.NewMessagesProcessor(memory.NewDatabase())
+		pr := process.NewMessagesProcessor(storage)
 		for _, expect := range testTable {
+
+			msgPayment := &model.MessagePayment{}
+			_ = json.Unmarshal([]byte(expect.Msg), msgPayment)
 
 			expect := expect
 
 			if err := pr.PaymentProcessor([]byte(expect.Msg)); err != nil {
 				if expect.Error != true {
 					t.Error(fmt.Errorf(`result %v != %w`, expect.Error, err))
+				}
+			}
+
+			if model, err := storage.GetPaymentById(msgPayment.UidMessage); err != nil {
+				if expect.Result != model {
+					t.Error(fmt.Errorf(`result %v != %v`, expect.Result, model))
 				}
 			}
 		}
@@ -58,6 +70,20 @@ var TestParam = []struct {
 	},
 	{
 		Msg: `{
+			"TypeMessage": "canceled",
+			"UidMessage":  "1A"
+		}`,
+		Result: &model.Payment{
+			TypeMessage: "processed",
+			UidMessage:  "1A",
+			AddressFrom: "43234",
+			AddressTo:   "4124",
+			Amount:      5000,
+		},
+		Error: true,
+	},
+	{
+		Msg: `{
 			"TypeMessage": "created",
 			"UidMessage":  "2A",
 			"AddressFrom": "43224245",
@@ -87,6 +113,74 @@ var TestParam = []struct {
 			AddressFrom: "43224245",
 			AddressTo:   "41123424",
 			Amount:      500000,
+		},
+		Error: true,
+	},
+	{
+		Msg: `{
+			"TypeMessage": "created",
+			"UidMessage":  "3A",
+			"AddressFrom": "1234",
+			"AddressTo":   "1234",
+			"Amount":     100
+		}`,
+		Result: &model.Payment{
+			TypeMessage: "created",
+			UidMessage:  "3A",
+			AddressFrom: "1234",
+			AddressTo:   "1234",
+			Amount:      100,
+		},
+		Error: false,
+	},
+	{
+		Msg: `{
+			"TypeMessage": "canceled",
+			"UidMessage":  "3A",
+			"AddressFrom": "1234",
+			"AddressTo":   "1234",
+			"Amount":     100
+		}`,
+		Result: &model.Payment{
+			TypeMessage: "canceled",
+			UidMessage:  "3A",
+			AddressFrom: "1234",
+			AddressTo:   "1234",
+			Amount:      100,
+		},
+		Error: false,
+	},
+	{
+		Msg: `{
+			"TypeMessage": "processed",
+			"UidMessage":  "3A",
+			"AddressFrom": "1234",
+			"AddressTo":   "1234",
+			"Amount":     100
+		}`,
+		Result: &model.Payment{
+			TypeMessage: "processed",
+			UidMessage:  "3A",
+			AddressFrom: "1234",
+			AddressTo:   "1234",
+			Amount:      100,
+		},
+		Error: true,
+	},
+	{
+		Msg: `{
+			"TypeMessage": "processed",
+			"UidMessage":  "4A",
+			"AddressFrom": "1234",
+			"AddressTo":   "1234",
+			"Amount":     1000
+		}`,
+		Result: &model.Payment{
+			TypeMessage: "processed",
+			UidMessage:  "3A",
+			AddressFrom: "1234",
+			AddressTo:   "1234",
+			Amount:      1000,
 		},
 		Error: true,
 	},
